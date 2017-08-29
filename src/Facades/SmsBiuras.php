0505d589028cfe5346f2014e45c46531e848180a
Algirdas Dumskis <algirdasdum@gmail.com>
2017-08-29 14:59:15 +0300

    Added docblock and changed required in composer.json

--- src/Facades/SmsBiuras.php
+++ src/Facades/SmsBiuras.php
@@ -4,8 +4,16 @@ namespace Adumskis\SmsBiurasLaravel\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class SmsBiuras
+ * @package Adumskis\SmsBiurasLaravel\Facade
+ * @see \Adumskis\SmsBiurasPhp\SmsBiuras
+ */
 class SmsBiuras extends Facade
 {
+    /**
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return \Adumskis\SmsBiurasPhp\SmsBiuras::class;
